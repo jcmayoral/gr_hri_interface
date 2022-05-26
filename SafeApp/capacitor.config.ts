329d97de0ee18bb686918b6326c64ced1d5a43c3
Jose <jocamaba1989@gmail.com>
2022-05-26 18:59:47 -0500

    first commit

---
+++ SafeApp/capacitor.config.ts
@@ -0,0 +1,11 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SafeApp',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  linuxAndroidStudioPath: "/media/mount/programs/android-studio/bin/studio.sh"
+};
+
+export default config;
