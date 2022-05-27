03528ae503f0f21289bf5bc858dacb33744c8b58
Jose <jocamaba1989@gmail.com>
2022-05-27 14:57:01 -0500

    joystick

--- SafeApp/src/app/tab1/tab1.page.ts
+++ SafeApp/src/app/tab1/tab1.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { NavController} from '@ionic/angular';
+
+
+//import { create } from 'nipplejs';
+import {create} from 'nipplejs';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +11,53 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  size :number = 50;
 
-  constructor() {}
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  ngAfterViewInit() {
+    console.log(document.getElementById('zone_joystick'));
+
+    var options = {
+        zone: document.getElementById('zone_joystick'),
+        size: 2 * this.size
+    };
+
+    var manager = create(options);
+
+    manager.on('added', function (evt, nipple) {
+      console.log('added');
+      console.log(nipple.angle, nipple.direction, nipple.force, nipple.force, nipple.position, nipple.pressure)
+      console.log(nipple.raw, nipple.vector)
+    })
+
+    manager.on("move", function(evt,data){
+      console.log("move", data.angle)
+    })
+      /*
+      nipple.on('move', function (evt, data) {
+
+        if(data.angle) {
+          if(Math.abs(data.force) <= 1) {
+            // px, py are between 0 and 1
+            let px = +Math.sin(data.angle.radian) * data.force;
+            let py = -Math.cos(data.angle.radian) * data.force;
+
+            console.log('px: ' + px);
+            console.log('py: ' + py);
+          }
+        }
+
+      });
+      
+      }).on('removed', function (evt, nipple) {
+        console.log('removed');
+        nipple.off('move');
+    });
+    */
+  }
+
 
 }
