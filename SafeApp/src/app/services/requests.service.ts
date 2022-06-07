6b8fd5885119c18a5a5e35465ea149a1f61423ab
Jose <jocamaba1989@gmail.com>
2022-06-07 00:06:44 -0500

    deta connection

--- SafeApp/src/app/services/requests.service.ts
+++ SafeApp/src/app/services/requests.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
+import {Http} from '@capacitor-community/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestsService {
-  endpoint = 'http://localhost:8000/'
+  //endpoint = 'http://192.168.1.67:8000/'
+  //endpoint = 'http://grassrobotics.sytes.net/'
+  endpoint = "https://jselkj.deta.dev/"
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json',
                                 'Cache-Control': 'no-cache'
@@ -21,7 +24,7 @@ export class RequestsService {
   constructor(private httpClient: HttpClient)  { }
 
   async lock(){
-    await this.post("lock/", {"lock": true})
+    await this.post3("lock/", {"lock": true})
     //this.get("/users/me")
   }
 
@@ -33,10 +36,44 @@ export class RequestsService {
       );
   }
 
-  async post(path:string, msg){
-    console.log("post "+ this.endpoint+path, msg)
-    this.httpClient.post(this.endpoint+path, msg, this.httpOptions).subscribe((data) => {
-       console.log(data)
+  async post2(route:string, msg:any){
+    const rawResponse = await fetch(this.endpoint+route, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({lock: 1})
+    }).catch(function(error){
+      alert(error)
+    });
+    //const content = await rawResponse.json();
+    //console.log(content);
+
+  }
+
+  async post3(route:string, msg:any){
+    console.log("post 3")
+    const rawResponse = await Http.post({
+      url: this.endpoint+route,
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify({lock: 1})
+    }).catch(function(error){
+      alert(error)
+    });
+    //const content = await rawResponse.json();
+    //console.log(content);
+
+  }
+
+  async post(route:string, msg){
+    console.log("post "+ this.endpoint+route, msg)
+    this.httpClient.post(this.endpoint+route, msg, this.httpOptions).subscribe((res : any) => {
+       console.log(res.specials)
     })
   }
 
