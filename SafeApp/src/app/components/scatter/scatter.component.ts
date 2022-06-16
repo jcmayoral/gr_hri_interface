dd1e26ab3e6d36934dec159b4b9540a5647b7138
Jose <jocamaba1989@gmail.com>
2022-06-16 15:36:51 -0500

    scatter working

--- SafeApp/src/app/components/scatter/scatter.component.ts
+++ SafeApp/src/app/components/scatter/scatter.component.ts
@@ -23,17 +23,21 @@ export class ScatterComponent implements AfterViewInit {
       datasets: [{
         label: 'Scatter Dataset',
         data: [{
-          x: -10,
-          y: 0
+          x: 2,
+          y: 2,
+          status: "A"
         }, {
           x: 0,
-          y: 10
+          y: 10,
+          status: "B"
         }, {
           x: 10,
-          y: 5
+          y: 5,
+          status: "C"
         }, {
           x: 0.5,
-          y: 5.5
+          y: 5.5,
+          status: "D"
         }],
         backgroundColor: 'rgb(255, 99, 132)'
       }]
@@ -42,9 +46,25 @@ export class ScatterComponent implements AfterViewInit {
   }
 
   makeScatter(){
-     const data = this.getData()
- 
-     this.scatterChart = new Chart(this.scatterCanvas.nativeElement, {
+    const datalabels = {
+      id: 'scatterDataLabels',
+      afterDatasetsDraw(chart, args, options){
+        const {ctx} = chart;
+        ctx.save()
+        ctx.font='20px sans-seriff'
+
+        for (let x = 0; x < chart.config.data.datasets.length; x++){
+          for (let i = 0; i < chart.config.data.datasets[x].data.length; i++){
+            //const textwidth = ctx.measureText(chart.config.data.datasets[x].data[i].status)
+            ctx.fillText(chart.config.data.datasets[x].data[i].status,
+              chart.getDatasetMeta(x).data[i].x ,
+              chart.getDatasetMeta(x).data[i].y)
+          }
+        }
+      }
+    }
+    const data = this.getData()
+    this.scatterChart = new Chart(this.scatterCanvas.nativeElement, {
       type: 'scatter',
       data: data,
       options: {
@@ -53,8 +73,9 @@ export class ScatterComponent implements AfterViewInit {
             type: 'linear',
             position: 'bottom'
           }
-        }
-      }
+        },
+      },
+      plugins: [datalabels]
     })
   }
 
