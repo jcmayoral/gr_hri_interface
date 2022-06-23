82af432e0e65439ba84356625c7ec59044b7af0e
Jose <jocamaba1989@gmail.com>
2022-06-23 15:27:48 -0500

    updates

--- SafeApp/src/app/components/map/map.component.ts
+++ SafeApp/src/app/components/map/map.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit, ViewChild, OnDestroy, AfterViewInit, ElementRef } from '@angular/core';
 import { ViewDidEnter } from '@ionic/angular';
 import * as L from 'leaflet';
+//import * as L from 'leaflet.gridlayer.googlemutant';
+import 'leaflet/dist/leaflet.css';
 import { antPath } from 'leaflet-ant-path';
 //import { ElementRef } from '@angular/core';
 import { GoogleMap } from '@capacitor/google-maps';
 
 
-const iconRetinaUrl = 'assets/icon/my-icon.png';
-const iconUrl = 'assets/icon/my-icon.png';
-const shadowUrl = 'assets/icon/my-icon.png';
+const iconRetinaUrl = 'assets/icon/marker-icon-2x.png';
+const iconUrl = 'assets/icon/marker-icon.png';
+const shadowUrl = 'assets/icon/marker-shadow.png';
 const iconDefault = L.icon({
   iconRetinaUrl,
   iconUrl,
   shadowUrl,
   iconSize: [2, 2],
   iconAnchor: [1.2, 1.2],
-  popupAnchor: [1, -34],
+  popupAnchor: [1, -1],
   tooltipAnchor: [16, -28],
   shadowSize: [41, 41]
 });
@@ -29,9 +31,12 @@ L.Marker.prototype.options.icon = iconDefault;
 })
 export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('map') mymap: ElementRef;
-  map: L.Map;
-  apiKey = "AIzaSyBltjbC3PeZfQP59CaTvzaYuR15kcxHBeQ";
+  @ViewChild('map2') mymap2: ElementRef;
 
+  map: L.Map;
+  newMap : GoogleMap;
+  apiKey = "AIzaSyBltjbC3PeZfQP59CaTvzaYuR15kcxHBeQ";
+  options: any
 
   constructor() { }
 
@@ -48,8 +53,8 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
 
   async googleMap(){
     const mapRef = document.getElementById('map');
-    const newMap = await GoogleMap.create({
-      id: 'my-map', // Unique identifier for this map instance
+    this.newMap = await GoogleMap.create({
+      id: 'map', // Unique identifier for this map instance
       element: mapRef, // reference to the capacitor-google-map element
       apiKey: this.apiKey, // Your Google Maps API Key
       config: {
@@ -116,20 +121,12 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
     //Leaflet.Marker([-150, 300]).addTo(this.map).bindPopup('Delhi').openPopup();
     Leaflet.marker([19.497391, -99.082275]).addTo(this.map).bindPopup('Leh').openPopup();
 
-    
-    //antPath([[19.397391, -99.182275], [34.1526, 77.5771]],
-    //  { color: '#FFFF00', weight: 50, opacity: 1.0 })
-    //  .addTo(this.map);
     */
-
-    this.map = L.map("map",{
-      zoomControl: true})
 
     var bounds = [
       [19.397391, -99.182275],
       [19.403516, -99.167656]
     ];
-    //this.map.fitBounds(bounds)
     console.log("aaaN", this.map, this.mymap)
 
     var url = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
@@ -143,21 +140,50 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
     //  }).addTo(this.map), 10000
     //)
 
-    await L.tileLayer(url, {
-      maxZoom: 20,
+    const roads = await L.tileLayer(url, {
+      //maxZoom: 80,
+      zoom: 5,
       //attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(this.map);
+    });
+    var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+    });
+
+    var streets = L.tileLayer(url, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1});
+
+    
+    this.map = L.map("map2",{
+      layers:[streets],
+      zoomControl: false}
+    )
+    
+    this.map.fitBounds(bounds)
+
 
     //this.map.dragging.disable()
     this.map.setView([19.402069, -99.171111],12);
-    //var marker = L.marker([19.402069, -99.171111])
+
+    /*
+    this.map.whenReady(() => {
+      setTimeout(() => {
+        this.map.invalidateSize();
+        console.log("errro")
+      }, 1000);
+    });
+
+    antPath([[19.397391, -99.182275], [34.1526, 77.5771]],
+      { color: '#FFFF00', weight: 50, opacity: 1.0 })
+      .addTo(this.map);
+    */
+
+    var marker = L.marker([19.402069, -99.171111])
     //var bounds = this.map.getBounds();
-    //this.map.setView([19.402069, -99.171111], 10);
+    this.map.setView([19.402069, -99.171111], 10);
     //marker.addTo(this.map);
-    //L.marker([19.402069, -99.171121]).addTo(this.map);
-    //var marker2 = new L.Marker(new L.LatLng(19.402069, -99.171131));
-    //this.map.addLayer(marker2);
-    // .bindPopup('Map Frame').openPopup();
+    L.marker([9.402069, -99.171121]).addTo(this.map);
+    var marker2 = new L.Marker(new L.LatLng(19.402069, -99.171131));
+    //this.map.addLayer(marker2).bindPopup('Map Frame').openPopup();
   }
 
   /** Remove map when we have multiple map object */
