24df0f2246d8d4ffa4373f1a1eb967b28cd26e16
Jose <jocamaba1989@gmail.com>
2022-06-24 15:35:54 -0500

    kind of working

--- SafeApp/src/app/components/voronoi/voronoi.component.ts
+++ SafeApp/src/app/components/voronoi/voronoi.component.ts
@@ -11,39 +11,10 @@ import * as d3 from 'd3';
   styleUrls: ['./voronoi.component.scss'],
 })
 export class VoronoiComponent implements OnInit {
-  GRIDSIZE = 50;
-  JITTER = 0;
-
-
-  width: number;
-  height: number;
-  margin = { top: 20, right: 20, bottom: 30, left: 40 };
   x: any;
   y: any;
   svg: any;
-  g: any;
-  link: any;
-  node: any;
-  simulation: any;
-
-  //d3
-  barData = [
-    { season: 'S1', viewers: 2500000 },
-    { season: 'S2', viewers: 3800000 },
-    { season: 'S3', viewers: 5000000 },
-    { season: 'S4', viewers: 6900000 },
-    { season: 'S5', viewers: 6900000 },
-    { season: 'S6', viewers: 7500000 },
-    { season: 'S7', viewers: 10000000 },
-    { season: 'S8', viewers: 17000000 }
-  ];
-  title = 'Game of Thrones';
-  subtitle = 'Viewers per season for';
-
-
   constructor() {
-    this.width = 500 - this.margin.left - this.margin.right;
-    this.height = 300 - this.margin.top - this.margin.bottom;
   }
 
   ngOnInit() {
@@ -56,8 +27,6 @@ export class VoronoiComponent implements OnInit {
     let points = [];
     for (let x = 0; x <= 5; x++) {
       for (let y = 0; y <= 5; y++) {
-        //points.push({x: x + this.JITTER * (Math.random() - Math.random()),
-          //            y: y + this.JITTER * (Math.random() - Math.random())});
         points.push({x: x, y: y});
       }
     }
@@ -65,9 +34,9 @@ export class VoronoiComponent implements OnInit {
   }
 
   scatter(data){
-    var margin = {top: 50, right: 50, bottom: -50, left: -50};
-    var width = this.width; //600 - margin.left - margin.right;
-    var height = this.height;//270 - margin.top - margin.bottom;
+    var margin = {top: 20, right: 0, bottom: 60, left: 0};
+    var width = 100 - margin.left - margin.right;
+    var height = 200 - margin.top - margin.bottom;
     //console.log("bbb", width, height)
 
     var xscale = d3.scaleLinear()
@@ -84,7 +53,8 @@ export class VoronoiComponent implements OnInit {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
+    
+    /*
     svg.append("g")        
     .attr("class", "x axis").call(xAxis);
 //    .attr("transform", "translate(0," + height + ")")
@@ -104,7 +74,7 @@ export class VoronoiComponent implements OnInit {
     .attr("dy", "1em")
     .style("text-anchor", "middle")
     .text("y");
-
+    */
 
     svg.selectAll("dot")
     .data(data)
