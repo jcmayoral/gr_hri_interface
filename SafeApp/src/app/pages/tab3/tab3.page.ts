be6f9ae93ac43a2ce61860befefc8f86ce3330a1
Jose Mayoral <jocamaba1989@gmail.com>
2022-07-08 15:44:06 -0500

    version1 automatic

--- SafeApp/src/app/pages/tab3/tab3.page.ts
+++ SafeApp/src/app/pages/tab3/tab3.page.ts
@@ -6,7 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  timer: any;
+  url: string;
 
-  constructor() {}
+  constructor() {
+    this.StartTimer()
+    this.url = "http://grassrobotics.sytes.net:8000/get_nav_feedback"
+
+  }
+
+  maxtime: any=30
+
+  StartTimer(){
+    this.timer = setTimeout(x => 
+      {
+        console.log("timer")
+        this.updateImage()
+        this.StartTimer();
+      }, 2000);
+  }
+
+  updateImage(){
+    var image = document.getElementById("feedback") as HTMLImageElement;
+    if(image.complete) {
+        image.src = this.url + '?_=' + new Date().getMilliseconds();         
+        console.log("a")  
+    }
+  }
 
 }
